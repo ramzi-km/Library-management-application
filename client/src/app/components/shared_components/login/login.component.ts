0b8b8693c453659e934f95bfa0038f1401700146
Ramzi K.M <ramzikm3@gmail.com>
2024-01-19 00:48:51 +0530

    :safety_vest: implement login page validations

--- client/src/app/components/shared_components/login/login.component.ts
+++ client/src/app/components/shared_components/login/login.component.ts
@@ -1,8 +1,47 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm!: FormGroup;
+  errMessage: string | null = null;
+  loading: boolean = false;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        this.noSpaceValidation,
+        Validators.pattern(/^(?=.*\d)(?=.*[a-zA-Z])/),
+      ]),
+    });
+  }
+  get loginFormControls() {
+    return this.loginForm.controls;
+  }
+
+  noSpaceValidation(control: AbstractControl): ValidationErrors | null {
+    let controlValue = control.value as string;
+    if (controlValue.indexOf(' ') >= 0) {
+      return { noSpaceValidator: true };
+    } else {
+      return null;
+    }
+  }
+  submitForm() {
+    this.loading = true;
+    console.log(this.loginForm.value);
+  }
+}
